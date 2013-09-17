5bafdb8df98a7c2e38c1e1d8efa2ad696422ae7f
Fabian <fromeroj@gmail.com>
2013-09-17 00:50:37 -0500

    tarea2

---
+++ programacion_avanzada/tarea2/tarea.h
@@ -0,0 +1,8 @@
+#ifndef inds_arreglo_
+#define inds_arreglo_
+
+i_arreglo* indices_arreglo_arreglo (int dim_proc, int dim_datos);
+void del_arreglo (i_arreglo *ids_a);
+void pinta_indices_arreglo_arreglo (i_arreglo *ids_a, int dim_proc);
+#endif // inds_arreglo_
+  
