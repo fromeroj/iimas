2bf2ef9efe055f60134a993acfe487088bb1552c
Fabian Romero <fromeroj@gmail.com>
2014-11-25 10:44:16 -0600

    avances tesis

--- acombinatorios/nqueens2.c
+++ acombinatorios/nqueens2.c
@@ -1,47 +1,50 @@
 #include <stdio.h> 
 #define max_n 50 
-
 int BOARD[max_n];
 int size;
+int Count;
 
 void print_sol(){ 
   int k;
+  printf("%8d:",Count);
   for(k=0;k<size;k++) printf("%4d",BOARD[k]+1); 
   printf("\n"); 
 } 
+
+void PrintCount(){ 
+  printf("The %d-queens problem has %d solutions.\n",size,Count); 
+}
 
 int no_conflictos(int k) 
 { 
   int i; 
   for(i=0;i<k;i++){ 
     if(BOARD[i]==BOARD[k] || abs(k-i)==abs(BOARD[k]-BOARD[i])) {
-      return(0);
+      return 0;
     }
   }
-  return(1); 
+  return 1; 
 }
 
 int queens(int current) 
 { 
   int k;
-  print_sol();
   if(current==size){
-    print_sol();
-    return(1);
+    Count++;
+    //    print_sol();
   }
   for(k=0;k<size;k++){
     BOARD[current]=k;
-    if(no_conflictos(current)){
-      if(queens(current+1))return(1);
-    }
+    if(no_conflictos(current) && queens(current+1) )return 1;
   }
-  return(0);
+  return 0;
 }
 
 int main(int argc, char **argv) 
 { 
   int k;
   size=(argc>1)?atoi(argv[1]):8;
-  for(k=0;k<size;k++)BOARD[k]=-1;
+  for(k=0;k<size;k++)BOARD[k]=0;
   queens(0);
+  PrintCount();
 }
