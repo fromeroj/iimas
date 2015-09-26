4c968edafb81d57d76988e71c1947d732060d7f5
Fabian Romero <fromeroj@gmail.com>
2015-09-26 05:26:30 -0500

    working with complete fields

--- maximumclique/mc.c
+++ maximumclique/mc.c
@@ -40,21 +40,44 @@ uint64_t* and(uint64_t* a,uint64_t* b){
   return c;
 }
 
+void addEdge(int i,int j){
+  if( i == j || i > size || j > size || i < 0 || j < 0)return;
+  graph[i*segments+j/64]|=0x1ULL<<(j%64);
+  graph[j*segments+i/64]|=0x1ULL<<(i%64);
+}
+
+int hasEdge(int i,int j){
+  if( i == j || i > size || j > size || i < 0 || j < 0)return 0;
+  return ((graph[i*segments+j/64] & 0x1ULL<<(j%64)) >0 )?1:0;
+}
+
+uint64_t* nbhd(int v){
+  return &graph[v*segments];
+}
+
+void printGraph(){
+  int i,j;
+  for(i=1;i<=size;i++){
+    for(j=1;j<=size;j++)printf("%d",hasEdge(i,j));
+    printf("\n");
+  }
+}
+
 void readGraph(char* fileName){
   FILE *inputfile=fopen(fileName, "r");
   size_t linesiz=0;
   char* linebuf=0;
   ssize_t linelen=0;
 
-  while (linelen=getline(&linebuf, &linesiz, inputfile)>0) {
+  while ( (linelen=getline(&linebuf, &linesiz, inputfile))>0) {
     if(linebuf[0]=='p'){
       int edges;
       char p[linesiz],edge[linesiz];
       sscanf(linebuf, "%s %s %d  %d", p, edge, &size, &edges );
-      segments=(((size-1)/64)+1);
+      segments=((size/64)+1);
       if(strcmp(edge,"edge")==0){
-	int x=(1+(size *(size-1)-1)/128);
-	graph=(uint64_t*)calloc(x, sizeof(uint64_t));
+	printf("Initializing graph with %d segments of size %d\n",size+1,segments);
+	graph=(uint64_t*)calloc((size+1)*segments, sizeof(uint64_t));
       }
     }
 
@@ -65,23 +88,6 @@ void readGraph(char* fileName){
       addEdge(v1,v2);
     }
   }
-}
-
-int address(int i,int j){
-  if(i>j){int k=i;i=j;j=k;}
-  return size*(size-1)/2 - (size-i+1)*(size-i)/2 + j - i -1;
-}
-
-void addEdge(int i,int j){
-  if(i==j)return;
-  int a=address(i,j);
-  graph[a/64]|=0x1ULL<<(a%64);
-}
-
-int hasEdge(int i,int j){
-  if(i==j)return 0;
-  int a=address(i,j);
-  return ((graph[a/64] & 0x1ULL<<(a%64)) >0 )?1:0;
 }
 
 char* printSet(uint64_t* c){
@@ -95,7 +101,7 @@ char* printSet(uint64_t* c){
   }
   strcat(s,":");
   for(i=1;i<=size;i++){
-    if((c[i/64] & 0x1ULL<<(i%64-1))>0){
+    if((c[i/64] & 0x1ULL<<(i%64))>0){
       sprintf(tmp,"%d,",i);
       strcat(s,tmp);
     }
@@ -105,36 +111,15 @@ char* printSet(uint64_t* c){
   return s;
 }
 
-void printGraph(){
-  int i,j;
-  for(i=1;i<=size;i++){
-    for(j=1;j<=size;j++)printf("%d",hasEdge(i,j));
-    printf("\n");
+void setBit(uint64_t* c,int index,int value){
+  if(index>size) return;
+  uint64_t base=(value==0)?0xFFFFFFFFFFFFFFFFULL:0x0ULL;
+  uint64_t val=(0x1ULL<<(index%64))^base;
+  if(value==0){
+    c[index/64]&=val;
+  }else{
+    c[index/64]|=val;
   }
-}
-
-int main ( int argc, char *argv[] )
-{
-  int i;
-  if ( argc != 2 )
-    {
-      printf( "usage: %s filename", argv[0] );
-    }
-  else
-    {
-      float start=startClock();
-      readGraph(argv[1]);
-      printGraph();
-      printf("size: %d\n",size);
-      allOff=(uint64_t*)calloc(segments, sizeof(uint64_t));
-      allOn=(uint64_t*)calloc(segments, sizeof(uint64_t));
-      for(i=0;i<segments-2;i++)allOn[i]=0xFFFFFFFFFFFFFFFF;
-      allOn[segments-1]=(0x1ULL<<(size%64))-1;
-      search();
-      int c =__builtin_popcount( solution );
-      printf("%d unos en la solucion %"PRIx64" %s \n ",c,solution[0],printSet(solution));
-      stopClock(start);
-    }
 }
 
 int popcount(uint64_t* c){
@@ -145,23 +130,8 @@ int popcount(uint64_t* c){
   return count;
 }
 
-void search(){
-  int i,operations=0;
-  uint64_t* c=and(allOff,allOff);
-  uint64_t* p=and(allOn,allOn);
-
-  printf("Start search: c:%" PRIx64 " p:%" PRIx64 " %s\n",c[0],p[0],printSet(p));
-  expand(c,p);
-}
-
-int getBit(uint64_t* c,int index){
-  if(index>size) return 0;
-  return ((c[index/64] & 0x1ULL<<(index%64))>0)?1:0;
-}
-
 int getIndexNthBit(uint64_t* c,int n){
-  //  printf("getIndexNthBit %" PRIx64 " -- %d\n",c[0],n);
-  if(n>size) return 0;
+  if(n>size) return -1;
   int i,j,count=0;
   for(i=0;i<segments;i++){
     if(count+__builtin_popcount(c[i])>=n){
@@ -179,52 +149,67 @@ int getIndexNthBit(uint64_t* c,int n){
   return -1;
 }
 
-void setBit(uint64_t* c,int index,int value){
-  if(index>size) return 0;
-  uint64_t base=(value==0)?0xFFFFFFFFFFFFFFFFULL:0x0ULL;
-  uint64_t val=0x1ULL<<(index%64-1)^base;
-  if(value==0){
-    c[index/64]&=val;
-  }else{
-    c[index/64]|=val;
-  }
-}
-
-
-
-uint64_t* nbhd(int v){
-  int i;
-  uint64_t* c=(uint64_t*)calloc(segments, sizeof(uint64_t));
-  for(i=1;i<=size;i++){
-    uint64_t val=hasEdge(i,v)?0x1ULL<<(i%64-1):0x0ULL;
-    c[i/64]|=val;
-  }
-  return c;
-}
-
-void expand(uint64_t* c,uint64_t* p){
-  int i;
-  printf("[[Expand]] p: %s c: %s)\n",printSet(p),printSet(c));
-//  (&operations)+;
-  for(i=popcount(p)-1;i>=0;i--){
-    if(popcount(c)+popcount(p)<=maxSize)return;
-    int v=getIndexNthBit(p,i+1)+1;
-    setBit(c,v,1);
-    uint64_t* np=and(p,nbhd(v));
-    printf("adding v:%d, Nbhd v:%s ->np:%s \n",v,printSet(nbhd(v)),printSet(np));
-    if(popcount(np)==0 && (popcount(c)>maxSize) )saveSolution(c);
-    if(popcount(np)>0){
-      printf("->");
-      expand(c,np);
-    }
-    setBit(c,v,0);
-    setBit(p,v,0);
-    printf("[[Retract %d]] p: %s c: %s)\n",v,printSet(p),printSet(c));
-  }
-}
-
 void saveSolution(uint64_t* c){
   solution=and(allOn,c);
   maxSize=popcount(c);
   printf("Solucion actual: %s \n",printSet(c));
+}
+
+void expand(uint64_t* c,uint64_t* p){
+  int i;
+  //printf("[[Expand]] p: %s c: %s)\n",printSet(p),printSet(c));
+  //  (&operations)+;
+  for(i=popcount(p);i>0;i--){
+    if(popcount(c)+popcount(p)<=maxSize)return;
+    int v=getIndexNthBit(p,i);
+    setBit(c,v,1);
+    uint64_t* np=and(p,nbhd(v));
+    //printf("adding v:%d, Nbhd v:%s ->np:%s \n",v,printSet(nbhd(v)),printSet(np));
+    if(popcount(np)==0 && (popcount(c)>maxSize) )saveSolution(c);
+    if(popcount(np)>0){
+      expand(c,np);
+    }
+    setBit(c,v,0);
+    setBit(p,v,0);
+    //printf("[[Retract %d]] p: %s c: %s)\n",v,printSet(p),printSet(c));
+  }
+}
+
+void search(){
+  uint64_t* c=and(allOff,allOff);
+  uint64_t* p=and(allOn,allOn);
+  p[0]=(p[0]>>1)<<1;
+  printf("Start search: c:%s p:%s\n",printSet(c),printSet(p));
+  expand(c,p);
+}
+
+int main ( int argc, char *argv[] )
+{
+  int i;
+  if ( argc != 2 )
+    {
+      printf( "usage: %s filename", argv[0] );
+    }
+  else
+    {
+      float start=startClock();
+      readGraph(argv[1]);
+      printGraph();
+      printf("size: %d\n",size);
+      allOff=(uint64_t*)calloc(segments, sizeof(uint64_t));
+      allOn=(uint64_t*)calloc(segments, sizeof(uint64_t));
+      for(i=0;i<segments-2;i++)allOn[i]=0xFFFFFFFFFFFFFFFFULL;
+      allOn[segments-1]=(0x1ULL<<(size%64+1))-1;
+      search();
+      int c =popcount( solution );
+      printf("%d elementos en la solucion %"PRIx64" %s \n ",c,solution[0],printSet(solution));
+      stopClock(start);
+    }
+  return 0;
+}
+
+
+int getBit(uint64_t* c,int index){
+  if(index>size) return 0;
+  return ((c[index/64] & 0x1ULL<<(index%64))>0)?1:0;
 }
