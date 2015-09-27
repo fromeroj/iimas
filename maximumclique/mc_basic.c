0993b43b8df3e4171f6c78d86b3d067d6723399c
Fabian Romero <fromeroj@gmail.com>
2015-09-27 00:28:31 -0500

    Clean up and comments

--- maximumclique/mc_basic.c
+++ maximumclique/mc_basic.c
@@ -5,85 +5,172 @@
 #include <string.h>
 #include <time.h>
 
-int size=0,maxSize=0,segments=1;
-uint64_t* solution;
-uint64_t* graph;
-uint64_t* allOff;
-uint64_t* allOn;
-
 #define ONE 0x1ULL
 #define ALL_ONES 0xFFFFFFFFFFFFFFFFULL
 
-float startClock(){
+/*   mc_basic  -- A very simple backtracking maximum clique algorithm
+ *
+ *   usage:     mc_basic filename, the filename has to be on the ascii format
+ *     for graphs used on DIMACS http://www.cs.hbg.psu.edu/txn131/clique.html
+ *   author    -- Fabian Romero
+ */
+
+/* graph is represented as an adjacency matrix.
+ * As the format is 1 based (the first element is 1 not 0), to simplify
+ * we keep unused the bit 0 of every adjacency list as well as the first row
+ * of the table.
+ * TODO: This could be a struct, but as we are not planning to manage several
+ * graphs at once and for convenience, we keep as a set of global variables. */
+int size=0;          /* the number of vertices of the graph */
+int max_size=0;       /* the size of the bigger cliquer found so far */
+int words=1;         /* how many uint64_t are required for a vertex's adjacency list*/
+uint64_t* graph;
+uint64_t* solution;  /* In the same adjaceny list format */
+uint64_t* all_off;    /* This is an adjacency list with all bits set */
+uint64_t* all_on;     /* This is an adjacency list with all bits unset */
+
+/*
+ *  Note: clock functions are for time keeping nothing especific for MC problem
+ */
+
+/*  star_clock: returns a float, with the "wall time marker" to compare at stop */
+float start_clock(){
   return (float)clock()/CLOCKS_PER_SEC;
 }
 
-float stopClock(float startTime){
+/*  stop_clock: returns and print the time ellapsed in seconds from the startTime
+ *  Parameters
+ *  startTime -- the starting time, start_clock() will provide this parameter */
+float stop_clock(float startTime){
   float endTime = (float)clock()/CLOCKS_PER_SEC;
   float elapsed = endTime - startTime;
   printf("Time elapsed: %fs\n",elapsed);
   return elapsed;
 }
 
+/*
+ * Functions for managing the adjacency lists
+ */
+
+/* intersection -- returns a NEW adjacency list having
+ * the intersection of bits of two adjacency lists
+ *
+ * Parameters
+ * a,b  -- pointers to the adjacency lists.
+ * Returns.
+ * a pointer to a NEWLY ALLOCATED adjacency list.       */
 uint64_t* intersection(uint64_t* a,uint64_t* b){
   int i;
-  uint64_t* c=(uint64_t*)calloc(segments, sizeof(uint64_t));
-  for(i=0;i<segments;i++){
+  uint64_t* c=(uint64_t*)calloc(words, sizeof(uint64_t));
+  for(i=0;i<words;i++){
     c[i] = a[i] & b[i];
   }
   return c;
 }
 
-void addEdge(int i,int j){
+/* get_bit -- returns 0 or 1 if the bit 'index'
+ *            is set on the adjacency list c
+ * Parameters
+ * c     -- pointer to the adjacency lists.
+ * index -- the elementh by index we want to know
+ * Returns.
+ * pointer to a *NEWLY ALLOCATED* adjacency list.  */
+int get_bit(uint64_t* c,int index){
+  if(index>size) return 0;
+  return ((c[index/64] & ONE<<(index%64))>0)?1:0;
+}
+
+/* set_bit -- set to 0 or 1 if the bit 'index'
+ *     is set on the adjacency list c
+ * Parameters
+ * c     -- pointer to the adjacency lists.
+ * index -- the elementh by index we want to know */
+void set_bit(uint64_t* c,int index,int value){
+  if(index>size) return;
+  uint64_t base=(value==0)?ALL_ONES:0x0ULL;
+  uint64_t val=(ONE<<(index%64))^base;
+  if(value==0){
+    c[index/64]&=val;
+  }else{
+    c[index/64]|=val;
+  }
+}
+
+/*
+ *  Functions to manage the adjacency matrix
+ */
+
+/* add_edge -- Adds an edge to the adjacency matrix
+ *             we store the FULL adjacency matrix, so, both (i,j)
+ *             AND (j,i) bits are set, also note is 1 based, so the
+ *             (0,x) and (x,0) bits are left zeroed in every case
+ * Parameters
+ * i,j     -- The indexes of the vertices to join.                   */
+void add_edge(int i,int j){
   if( i == j || i > size || j > size || i < 0 || j < 0)return;
-  graph[i*segments+j/64]|=ONE<<(j%64);
-  graph[j*segments+i/64]|=ONE<<(i%64);
+  graph[i*words+j/64]|=ONE<<(j%64);
+  graph[j*words+i/64]|=ONE<<(i%64);
 }
 
-int hasEdge(int i,int j){
+/* has_edge -- returns 0 or 1 should the matrix contain the edge (i,j)
+ *             note that (i,j) is there iff (j,i) is there
+ * Parameters
+ * i.j     -- The indexes of the vertices                            */
+int has_edge(int i,int j){
   if( i == j || i > size || j > size || i < 0 || j < 0)return 0;
-  return ((graph[i*segments+j/64] & ONE<<(j%64)) >0 )?1:0;
+  return ((graph[i*words+j/64] & ONE<<(j%64)) >0 )?1:0;
 }
 
+/* nbhd -- returns the part of the matrix beind the adjacency list of the
+ *         vertex v (not a copy, this is supposed to be used Read Only)
+ * Parameters
+ * v     -- The index of the vertex                                     */
 uint64_t* nbhd(int v){
-  return &graph[v*segments];
+  return &graph[v*words];
 }
 
-void printGraph(){
+/* print_graph -- utility function that displays on stdout the full adjacency
+ *  matrix of the graph                                                    */
+void print_graph(){
   int i,j;
   for(i=1;i<=size;i++){
-    for(j=1;j<=size;j++)printf("%d",hasEdge(i,j));
+    for(j=1;j<=size;j++)printf("%d",has_edge(i,j));
     printf("\n");
   }
 }
 
-
-void savePixmap(){
+/* save_pixmap -- utility function that saves an image on xpm format of the
+ *  matrix of adjacency the graph on the file '/tmp/sol.xpm'                    */
+void save_pixmap(){
   int i,j;
   char c;
-  FILE *fp = fopen("sol.xpm", "w");
-  if (fp == NULL)
-    {
-      printf("Error opening file!\n");
-      exit(1);
-    }
-  fprintf(fp, "! XPM2\n%d %d 3 1\nw c #FFFFFF\nb c #000000\nr c #FF0000\n",size,size);
+  FILE *fp = fopen("/tmp/sol.xpm", "w");
+  if (fp == NULL) exit(1);
+  fprintf(fp, "/* XPM */\nstatic char * XFACE[] = {\n\"%d %d 3 1\",\n",size,size);
+  fprintf(fp, "\n\"w c #FFFFFF\",\n\"g c #555555\",\n\"r c #FF0000\",\n");
   for(i=1;i<=size;i++){
+    putc('"',fp);
     for(j=1;j<=size;j++){
-      if(hasEdge(i,j)){
-	c=(((solution[i/64] & ONE<<(i%64)) >0) &&
-	   ((solution[j/64] & ONE<<(j%64)) >0))?'r':'b';
-      }else{
+      if(has_edge(i,j))
+	c=(get_bit(solution,i) && get_bit(solution,j))?'r':'g';
+      else
 	c='w';
-      }
       putc(c,fp);
     }
+    putc('"',fp);
+    if(i<size)putc(',',fp);
     putc('\n',fp);
   }
+    putc('}',fp);
+    putc(';',fp);
   fclose(fp);
 }
 
-void readGraph(char* fileName){
+/* read_graph -- This function will read the graph from a file on the DIMACS format
+                 and initialize all other graph wide variables
+		 (size, max_size, all_on,all_off)
+ *  matrix of adjacency the graph on the file '/tmp/sol.xpm'                    */
+void read_graph(char* fileName){
   FILE *inputfile=fopen(fileName, "r");
   size_t linesiz=0;
   char* linebuf=0;
@@ -94,10 +181,10 @@ void readGraph(char* fileName){
       int edges;
       char p[linesiz],edge[linesiz];
       sscanf(linebuf, "%s %s %d  %d", p, edge, &size, &edges );
-      segments=((size/64)+1);
+      words=((size/64)+1);
       if(strcmp(edge,"edge")==0){
-	printf("Initializing graph with %d segments of size %d\n",size+1,segments);
-	graph=(uint64_t*)calloc((size+1)*segments, sizeof(uint64_t));
+	printf("Initializing graph with %d words of size %d\n",size+1,words);
+	graph=(uint64_t*)calloc((size+1)*words, sizeof(uint64_t));
       }
     }
 
@@ -105,7 +192,7 @@ void readGraph(char* fileName){
       int v1,v2;
       char p[linesiz];
       sscanf(linebuf, "%s %d  %d", p, &v1, &v2 );
-      addEdge(v1,v2);
+      add_edge(v1,v2);
     }
   }
 }
@@ -113,7 +200,7 @@ void readGraph(char* fileName){
 void printSet(uint64_t* c,int newLine){
   int i;
   printf("{");
-  for(i=0;i<segments;i++){
+  for(i=0;i<words;i++){
     printf("%"PRIx64,c[i]);
   }
   printf(":");
@@ -126,27 +213,17 @@ void printSet(uint64_t* c,int newLine){
   if(newLine)printf("\n");
 }
 
-void setBit(uint64_t* c,int index,int value){
-  if(index>size) return;
-  uint64_t base=(value==0)?ALL_ONES:0x0ULL;
-  uint64_t val=(ONE<<(index%64))^base;
-  if(value==0){
-    c[index/64]&=val;
-  }else{
-    c[index/64]|=val;
-  }
-}
 
 int popcount(uint64_t* c){
   int i,count=0;
-  for(i=0; i<segments; i++) count+=__builtin_popcountll(c[i]);
+  for(i=0; i<words; i++) count+=__builtin_popcountll(c[i]);
   return count;
 }
 
 int getIndexNthBit(uint64_t* c,int n){
   if(n>size) return -1;
   int i,j,count=0;
-  for(i=0;i<segments;i++){
+  for(i=0;i<words;i++){
     if(count+__builtin_popcountll(c[i])>=n){
       for(j=0;j<64;j++){
 	count+=((c[i] & ONE<<j)>0)?1:0;
@@ -162,8 +239,8 @@ int getIndexNthBit(uint64_t* c,int n){
 
 void saveSolution(uint64_t* c){
   free(solution);
-  solution=intersection(allOn,c);
-  maxSize=popcount(c);
+  solution=intersection(all_on,c);
+  max_size=popcount(c);
   printf("Solucion actual: ");
   printSet(c,1);
 }
@@ -173,25 +250,25 @@ void expand(uint64_t* c,uint64_t* p){
   //  printf("[[Expand]] p: %s c: %s)\n",printSet(p),printSet(c));
   //  (&operations)+;
   for(i=popcount(p);i>0;i--){
-    if(popcount(c)+popcount(p)<=maxSize)return;
+    if(popcount(c)+popcount(p)<=max_size)return;
     int v=getIndexNthBit(p,i);
-    setBit(c,v,1);
+    set_bit(c,v,1);
     uint64_t* np=intersection(p,nbhd(v));
     //    printf("adding v:%d, Nbhd v:%s ->np:%s \n",v,printSet(nbhd(v)),printSet(np));
-    if(popcount(np)==0 && (popcount(c)>=maxSize) )saveSolution(c);
+    if(popcount(np)==0 && (popcount(c)>=max_size) )saveSolution(c);
     if(popcount(np)>0){
       expand(c,np);
     }
     free(np);
-    setBit(c,v,0);
-    setBit(p,v,0);
+    set_bit(c,v,0);
+    set_bit(p,v,0);
     //printf("[[Retract %d]] p: %s c: %s)\n",v,printSet(p),printSet(c));
   }
 }
 
 void search(){
-  uint64_t* c=intersection(allOff,allOff);
-  uint64_t* p=intersection(allOn,allOn);
+  uint64_t* c=intersection(all_off,all_off);   /* Using intersection for copy */
+  uint64_t* p=intersection(all_on,all_on);
   p[0]=(p[0]>>1)<<1;
   printf("Start search: c:");
   printSet(c,0);
@@ -205,26 +282,21 @@ int main ( int argc, char *argv[] ){
   if ( argc != 2 ){
     printf( "usage: %s filename", argv[0] );
   }else{
-    float start=startClock();
-    readGraph(argv[1]);
-    //printGraph();
+    float start=start_clock();
+    read_graph(argv[1]);
+    //print_graph();
     printf("size: %d\n",size);
-    allOff=(uint64_t*)calloc(segments, sizeof(uint64_t));
-    allOn=(uint64_t*)calloc(segments, sizeof(uint64_t));
-    for(i=0;i<segments-1;i++)allOn[i]=ALL_ONES;
-    allOn[segments-1]=(size%64==63)?ALL_ONES:(ONE<<(size%64+1))-1;
+    all_off=(uint64_t*)calloc(words, sizeof(uint64_t));
+    all_on=(uint64_t*)calloc(words, sizeof(uint64_t));
+    for(i=0;i<words-1;i++)all_on[i]=ALL_ONES;
+    all_on[words-1]=(size%64==63)?ALL_ONES:(ONE<<(size%64+1))-1;
     search();
     int c =popcount( solution );
     printf("solution %"PRIx64" \n %d elements\n",solution[0],c);
     printSet(solution,1);
     printf("solution %"PRIx64" \n %d elements\n",solution[0],c);
-    savePixmap();
-    stopClock(start);
+    save_pixmap();
+    stop_clock(start);
   }
   return 0;
-}
-
-int getBit(uint64_t* c,int index){
-  if(index>size) return 0;
-  return ((c[index/64] & ONE<<(index%64))>0)?1:0;
 }
